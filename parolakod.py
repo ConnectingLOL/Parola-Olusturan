d3e44651d48d7f3d020e7ec82b3fb55612bd60d5
ConnectingLOL <lutfiberksak@gmail.com>
2024-10-27 21:48:10 +0300

    Update parolakod.py

--- parolakod.py
+++ parolakod.py
@@ -1,10 +1,7 @@
 import random
-
 elemanlar = "+-/*!&$?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-uzunluk = int(input("Şifrenin uzunluğu giriniz: "))
+uzunluk = int(input("Şifrenizin uzunluğunu girin: "))
 x = ""
-
 for i in range(uzunluk):
-    x = random.randint(elemanlar)
-
+    x += random.choice(elemanlar)
 print(x)
